b2276c4da68a03539e5cd8b2b4758c3c25dea6b2
Alex030104 <a.perezp@uniandes.edu.co>
2023-10-22 22:26:29 -0500

    Typescript1

---
+++ js/serie.js
@@ -0,0 +1,14 @@
+Object.defineProperty(exports, "module", { value: true });
+exports.serie = void 0;
+class serie {
+    constructor(id, name, channel, seasons, description, link, image) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.description = description;
+        this.link = link;
+        this.image = image;
+    }
+}
+exports.serie = serie;
